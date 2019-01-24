662d332f54aafce75c1d28104cfe2078886f16ba
Daniel J Walsh <dwalsh@redhat.com>
2019-01-24 06:03:13 +0100

    Cleanup script to allow colors, ctrl-C, and clear screen
    Also add firefox display to show web service is running.
    
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- running/generatekube/README.md
+++ running/generatekube/README.md
@@ -3,3 +3,5 @@
 This demo demonstrates how to use the `podman generate kube` command.  It requires
 a functioning Kubernetes environment and will use the `kubectl` command to create
 and remove pods and containers.
+
+Requires podman, kubctl, firefox
--- running/generatekube/kubedemo.sh
+++ running/generatekube/kubedemo.sh
@@ -1,18 +1,44 @@
 #!/bin/bash
 
+trap 'exit 0' INT
+
+# Setting up some colors for helping read the demo output.
+# Comment out any of the below to turn off that color.
+bold=$(tput bold)
+blue=$(tput setaf 4)
+reset=$(tput sgr0)
+
+read_color() {
+    read -p "${bold}$1${reset}"
+}
+
+echo_color() {
+    echo "${blue}$1${reset}"
+}
 
 setup () {
     # Verify podman exists
     if ! hash podman 2>/dev/null; then
-        echo "Unable to find the podman commmand"
-        exit 1
+	echo "Unable to find the podman commmand"
+	exit 1
     fi
 
     # Verify kubectl exists
     if ! hash kubectl 2>/dev/null; then
-        echo "Unable to find the kubectl commmand"
-        exit 1
+	echo "Unable to find the kubectl commmand"
+	exit 1
     fi
+    # Verify firefox exists
+    if ! hash firefox -h 2>/dev/null; then
+	echo "Unable to find the firefox commmand"
+	exit 1
+    fi
+    sudo systemctl restart crio
+    rc=$?; if [[ $rc != 0 ]]; then
+	echo "Unable to start the crio daemon"
+	exit 1
+    fi
+
     sudo podman image exists quay.io/baude/demodb:latest
     rc=$?; if [[ $rc != 0 ]]; then sudo podman pull quay.io/baude/demodb:latest; fi
     sudo podman image exists quay.io/baude/demogen:latest
@@ -20,6 +46,8 @@ setup () {
     sudo podman image exists quay.io/baude/demoweb:latest
     rc=$?; if [[ $rc != 0 ]]; then sudo podman pull quay.io/baude/demoweb:latest; fi
 
+    sudo podman stop -t 0 demodb demogen demoweb 2>/dev/null
+    sudo podman rm -f demodb demogen demoweb 2>/dev/null
     mkdir -p /tmp/kube-container
     mkdir -p /tmp/kube-pod
 }
@@ -35,48 +63,57 @@ cleanKube() {
 }
 
 tailContainer () {
-    read -p "$ sudo podman logs -lf"
+    echo_color "Execute ^c to exit the log"
+    echo ""
+    read_color "$ sudo podman logs -lf"
     while [ 0 -eq 0 ]
     do
-        trap break INT
-        sudo podman logs -lf
+	trap break INT
+	sudo podman logs -lf
     done
     echo ""
+    trap 'exit 0' INT
+    clear
 }
 
 watchPods() {
-    read -p "$ sudo kubectl get pods --watch "
+    read_color "$ sudo kubectl get pods --watch "
     while [ 0 -eq 0 ]
     do
-        trap break INT
-        sudo kubectl get pods --watch
+	trap break INT
+	sudo kubectl get pods --watch
     done
     echo ""
+    trap 'exit 0' INT
 }
 
 runCommand() {
     echo ""
-    read -p "$ $1"
+    read_color "$ $1"
     $1
     echo ""
+    read -p "Continue"
+    clear
 }
 
 runPipeCommand() {
     echo ""
-    read -p "$ $1 > $2"
+    read_color "$ $1 > $2"
     $1 > $2
     echo ""
+    read -p "Continue"
+    clear
 }
 
 sendMessage() {
     echo ""
-    echo "$1"
+    echo_color "$1"
     echo ""
 }
 
 sendMessageWait() {
     echo ""
-    read -p "$1"
+    read_color "$1"
     echo ""
 }
 
@@ -110,7 +147,12 @@ runCommand "sudo podman container runlabel run quay.io/baude/demoweb:latest"
 sendMessage "Check the logs to make sure the generator is running"
 tailContainer
 
-
+##
+## Use firefox to connect to web service
+##
+sendMessage "Connect to web-client using firefox"
+web=$(sudo podman logs -l | grep http | cut -f 5 -d ' ')
+runCommand "firefox $web"
 
 ##
 ## Show our containers
@@ -129,6 +171,7 @@ runCommand "sudo podman generate kube demodb"
 sendMessage "Run for each container and pipe to a file"
 sendMessage "Generate Kubernetes YAML for 'demodb'"
 runPipeCommand "sudo podman generate kube demodb" "/tmp/kube-container/demodb.yaml"
+
 sendMessage "Generate Kubernetes YAML for 'demogen'"
 runPipeCommand "sudo podman generate kube demogen" "/tmp/kube-container/demogen.yaml"
 sendMessage "Generate Kubernetes YAML and a service for 'demoweb'"
