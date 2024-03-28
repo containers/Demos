6052aee59b2092bd2ff5ab5f6cd37e54b46b07ce
Daniel J Walsh <dwalsh@redhat.com>
2024-03-28 16:51:38 -0400

    Update to use new ai-lab-recipes
    Signed-off-by: Daniel J Walsh <dwalsh@redhat.com>

--- bootc/inference/podman.sh
+++ bootc/inference/podman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEFAULT_APP="inference"
+export DEFAULT_APP="inference"
 
 . ../podman_helper.sh
 
@@ -13,7 +13,7 @@ Build bootable OCI Image"
 
     exec_color "podman manifest exists ${IMAGE} && podman manifest rm ${IMAGE} || podman rmi --force ${IMAGE}"
 
-    exec_color "podman build --security-opt label=disable -v $HOME/.local/share/containers:/tmp/containers -v $XDG_RUNTIME_DIR/containers/auth.json:/run/containers/0/auth.json:z --no-cache --cap-add SYS_ADMIN --build-arg TAG=1.0 --build-arg \"SSHPUBKEY=$(cat $HOME/.ssh/id_rsa.pub)\"  --build-arg=\"REGISTRY=$REGISTRY\" --arch=${ARCH} $FROM--manifest ${IMAGE} $APP/"
+    exec_color "podman build --security-opt label=disable -v $HOME/.local/share/containers:/tmp/containers -v $XDG_RUNTIME_DIR/containers/auth.json:/run/containers/0/auth.json --no-cache --cap-add SYS_ADMIN --build-arg TAG=1.0 --build-arg \"SSHPUBKEY=$(cat $HOME/.ssh/id_rsa.pub)\"  --build-arg=\"REGISTRY=$REGISTRY\" --arch=${ARCH} $FROM--manifest ${IMAGE} $APP/"
 }
 
 function clone_ai {
@@ -25,13 +25,13 @@ Clone AI and explore what is available for use with the Podman Desktop AI Studio
 
     exec_color "podman push ${REGISTRY}/mymodel:1.0"
 
-    exec_color "podman build -t ${REGISTRY}/playground:1.0 ai-lab-recipes/playground"
+    exec_color "podman build -t ${REGISTRY}/model_server:1.0 -f ai-lab-recipes/model_servers/llamacpp_python/base/Containerfile ai-lab-recipes/model_servers/llamacpp_python/"
 
-    exec_color "podman push ${REGISTRY}/playground:1.0"
+    exec_color "podman push ${REGISTRY}/model_server:1.0"
 
-    exec_color "podman build -t ${REGISTRY}/rag-langchain:1.0 -f ai-lab-recipes/chatbot-langchain/builds/Containerfile ai-lab-recipes/chat-langchain"
+    exec_color "podman build -t ${REGISTRY}/chatbot:1.0 -f ai-lab-recipes/recipes/natural_language_processing/chatbot/builds/Containerfile ai-lab-recipes/recipes/natural_language_processing/chatbot"
 
-    exec_color "podman push ${REGISTRY}/rag-langchain:1.0"
+    exec_color "podman push ${REGISTRY}/chatbot:1.0"
 }
 
 
